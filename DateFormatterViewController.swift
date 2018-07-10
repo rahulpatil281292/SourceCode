6bd76f926522b247680cf878fbd72315161c2960
rahulpatil281292 <32728504+rahulpatil281292@users.noreply.github.com>
2018-07-10 20:08:09 +0530

    Date formatting in date.

--- DateFormatterViewController.swift
+++ DateFormatterViewController.swift
@@ -32,7 +32,28 @@ class ViewController: UIViewController {
 }
 
 
+/*
 
+Tuesday, Jul 10, 2018	
+EEEE, MMM d, yyyy
+07/10/2018	
+MM/dd/yyyy
+07-10-2018 14:36	
+MM-dd-yyyy HH:mm
+Jul 10, 2:36 PM	
+MMM d, h:mm a
+July 2018	
+MMMM yyyy
+Jul 10, 2018	
+MMM d, yyyy
+Tue, 10 Jul 2018 14:36:55 +0000	
+E, d MMM yyyy HH:mm:ss Z
+2018-07-10T14:36:55+0000	
+yyyy-MM-dd'T'HH:mm:ssZ
+10.07.18	
+dd.MM.yy
+
+*/
 
 
 
