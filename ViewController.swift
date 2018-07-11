0ba00c8f662e6142167e06313a5be11454c5b5e7
rahulpatil281292 <32728504+rahulpatil281292@users.noreply.github.com>
2018-07-11 14:08:57 +0530

    Navigate from source to destination

---
+++ ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by ITC Infotech on 05/07/17.
+//  Copyright © 2018 Rahul Patil. All rights reserved.
+//
+
+import UIKit
+
+// We can navigate from source viewcontroller to destination viewcontroller in 3 way's
+//  1 - Using present , 2 - Using push, 3 - Using Segue
+// we can send data with above 3 ways from source to destination
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func nextButtonPressed(_ sender : UIButton) {
+        usingSegue()
+    }
+    
+    func usingPush()  {
+        // push viewcontroller not workin on navigationcontroller.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let viewcontroller = storyboard.instantiateViewController(withIdentifier: "nextviewcontroller") as? NextViewController {
+            viewcontroller.name = "RAhul patil"
+            self.navigationController?.present(viewcontroller, animated: true, completion: nil)
+           // self.navigationController?.pushViewController(viewcontroller, animated: true)
+        }
+    }
+    
+    func usingSegue()  {
+        // navigate second veiw controller by using segue
+        // if we navigate by using segue then can't dismiss view controller
+        self.performSegue(withIdentifier: "nextsegue", sender: self)
+    }
+    
+    // overide method to send data from source to destination.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? NextViewController {
+            vc.name = "rahul patil"
+        }
+    }
+    
+    func usingPresent()  {
+        // if your viewController dont have navigationController then present it.
+        // if you are working on different storyboard then you should present viewcontroller.
+        let getStroryboard = UIStoryboard.init(name: "Main", bundle: nil)
+        if let getViewcontroller = getStroryboard.instantiateViewController(withIdentifier: "nextviewcontroller") as? NextViewController {
+        getViewcontroller.name = "Rahul Patil"
+        self.present(getViewcontroller, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func previousViewControllerBtnPressed(_ sender : UIButton) {
+        // dismiss view controller worked on present and push view controller.
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
